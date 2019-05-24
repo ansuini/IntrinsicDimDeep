85d6b03fa32f52040d82f41d2db93b10c5bf8c62
ansuini <alessioansuini@gmail.com>
2019-05-24 16:39:18 +0200

    up

--- scripts/mnist/analyze.py
+++ scripts/mnist/analyze.py
@@ -42,9 +42,6 @@ parser.add_argument('--dataset', metavar='DATASET',
 parser.add_argument('--epochs', default=0, type=int, metavar='N',
                     help='number of total epochs to run (set it equal to the number of training epochs of your model)')
 
-parser.add_argument('--id_all_layers', default=0, type=int, metavar='',
-                    help='compute ID in all layers at end of training')
-
 parser.add_argument('--fraction', default=0.9, type=float, metavar='fraction',
                     help='fraction of data resampling for error estimation')
 
@@ -55,16 +52,13 @@ parser.add_argument('--nres', default=50, type=int, metavar='N',
 args = parser.parse_args()
 dataset = args.dataset
 epochs = args.epochs
-extract = args.extract
 fraction = args.fraction
 nres = args.nres
-id_all_layers = args.id_all_layers
 
 method = 'euclidean'
 verbose=False
 
 RES = join(ROOT, 'data', dataset, 'results')
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 kwargs = {'num_workers': 2, 'pin_memory': True} if device.type == 'cuda' else {}
 
@@ -73,7 +67,6 @@ print(kwargs)
 print('Results will be saved in {}'.format(RES))
 
 # load sample (the sample is the same for mnist and mnist_shuffled)
-
 
 sample = torch.load(join(ROOT,'data', dataset, 'results', 'sample.pt') )
 nsamples = sample[0].shape[0]
@@ -95,45 +88,41 @@ def computeID(r,epoch,nres,fraction):
 
     
 #ID at all layers
-if id_all_layers:
-    epoch = epochs-1
-    print('Extracting representations from all layers...')
-    # load model
-    model = torch.load(join(RES, 'model_' + str(epoch) + '.pt') )
 
-    # extract representations from the sample
-    out1, out2, out3, out4, out5, out6 = model.extract_all(sample[0].to(device),verbose=verbose)
-    out1 = out1.view(nsamples, -1).cpu().data
-    out2 = out2.view(nsamples, -1).cpu().data
-    out3 = out3.view(nsamples, -1).cpu().data
-    out4 = out4.view(nsamples, -1).cpu().data 
-    out5 = out5.view(nsamples, -1).cpu().data
-    out6 = out6.view(nsamples, -1).cpu().data 
-    
-    
-    torch.save(out1, join(RES, 'All_R1') )
-    torch.save(out2, join(RES, 'All_R2') )
-    torch.save(out3, join(RES, 'All_R3') )
-    torch.save(out4, join(RES, 'All_R4') )
-    torch.save(out5, join(RES, 'All_R5') )
-    torch.save(out6, join(RES, 'All_R6') )
-   
-    print('Done.')
+epoch = epochs-1
+print('Extracting representations from all layers...')
+# load model
+model = torch.load(join(RES, 'model_' + str(epoch) + '.pt') )
 
-    # ID of all layers
+# extract representations from the sample
+out1, out2, out3, out4, out5, out6 = model.extract_all(sample[0].to(device),verbose=verbose)
+out1 = out1.view(nsamples, -1).cpu().data
+out2 = out2.view(nsamples, -1).cpu().data
+out3 = out3.view(nsamples, -1).cpu().data
+out4 = out4.view(nsamples, -1).cpu().data 
+out5 = out5.view(nsamples, -1).cpu().data
+out6 = out6.view(nsamples, -1).cpu().data 
+  
+torch.save(out1, join(RES, 'All_R1') )
+torch.save(out2, join(RES, 'All_R2') )
+torch.save(out3, join(RES, 'All_R3') )
+torch.save(out4, join(RES, 'All_R4') )
+torch.save(out5, join(RES, 'All_R5') )
+torch.save(out6, join(RES, 'All_R6') )
+print('Done.')
+
+# ID of all layers
     
-    fname = join(RES, 'ID_all')
+fname = join(RES, 'ID_all')
    
-    ID_all = []       
-    ID_all.append(computeID(sample[0].view(sample[0].shape[0],-1),epoch,nres,fraction))
-    # ID of all other layers
-    for j in range(1,len(layers)):
-        
-        r = torch.load(join(RES, 'All_R' + str(j)) ) 
-        
-        ID_all.append(computeID(r,epoch,nres,fraction)) 
-    ID_all = np.array(ID_all)
-    np.save(fname,ID_all)
-    print('Final result: {}'.format(ID_all[:,0]))
-    print('Done.')
+ID_all = []       
+ID_all.append(computeID(sample[0].view(sample[0].shape[0],-1),epoch,nres,fraction))
+# ID of all other layers
+for j in range(1,len(layers)):
+    r = torch.load(join(RES, 'All_R' + str(j)) ) 
+    ID_all.append(computeID(r,epoch,nres,fraction)) 
+ID_all = np.array(ID_all)
+np.save(fname,ID_all)
+print('Final result: {}'.format(ID_all[:,0]))
+print('Done.')
     
